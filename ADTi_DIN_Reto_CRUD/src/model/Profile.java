58e8158948dfdfc6898b5c10a051f79abcd6fe18
Ekasestao <ekaitzcampo2002@gmail.com>
2025-10-14 10:49:39 +0200

    Creating models

---
+++ ADTi_DIN_Reto_CRUD/src/model/Profile.java
@@ -0,0 +1,92 @@
+package model;
+
+/**
+ *
+ * @author 2dami
+ */
+public abstract class Profile {
+    protected int p_id;
+    protected String p_email;
+    protected String p_username;
+    protected String p_password;
+    protected String p_name;
+    protected String p_lastname;
+    protected String p_telephone;
+
+    public Profile(String p_email, String p_username, String p_password, String p_name, String p_lastname, String p_telephone) {
+        this.p_email = p_email;
+        this.p_username = p_username;
+        this.p_password = p_password;
+        this.p_name = p_name;
+        this.p_lastname = p_lastname;
+        this.p_telephone = p_telephone;
+    }
+    
+    public Profile(int p_id, String p_email, String p_username, String p_password, String p_name, String p_lastname, String p_telephone) {
+        this.p_id = p_id;
+        this.p_email = p_email;
+        this.p_username = p_username;
+        this.p_password = p_password;
+        this.p_name = p_name;
+        this.p_lastname = p_lastname;
+        this.p_telephone = p_telephone;
+    }
+    
+    public int getId() {
+        return p_id;
+    }
+    
+    public void setId(int p_id) {
+        this.p_id = p_id;
+    }
+
+    public String getEmail() {
+        return p_email;
+    }
+
+    public void setEmail(String p_email) {
+        this.p_email = p_email;
+    }
+
+    public String getUsername() {
+        return p_username;
+    }
+
+    public void setUsername(String p_username) {
+        this.p_username = p_username;
+    }
+
+    public String getPassword() {
+        return p_password;
+    }
+
+    public void setPassword(String p_password) {
+        this.p_password = p_password;
+    }
+
+    public String getName() {
+        return p_name;
+    }
+
+    public void setName(String p_name) {
+        this.p_name = p_name;
+    }
+
+    public String getLastname() {
+        return p_lastname;
+    }
+
+    public void setLastname(String p_lastname) {
+        this.p_lastname = p_lastname;
+    }
+
+    public String getTelephone() {
+        return p_telephone;
+    }
+
+    public void setTelephone(String p_telephone) {
+        this.p_telephone = p_telephone;
+    }
+
+    public abstract String show();
+}
---
+++ ADTi_DIN_Reto_CRUD/src/model/User.java
@@ -0,0 +1,9 @@
+package model;
+
+/**
+ *
+ * @author 2dami
+ */
+public class User {
+    
+}
