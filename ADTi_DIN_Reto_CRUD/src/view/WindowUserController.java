1d19d70480c7ee80c1bdffef6049cda58b0febc1
Enchufe0 <sabatelvictor96@gmail.com>
2025-10-14 09:53:39 +0200

    Windows and controllers created.

---
+++ ADTi_DIN_Reto_CRUD/src/view/WindowUserController.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package view;
+
+import controller.Controller;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+
+/**
+ *
+ * @author 2dami
+ */
+public class WindowUserController implements Initializable {
+    private Controller controller;
+    
+    @FXML
+    private Label label;
+    
+    
+    /**
+     * Asigna el controlador principal.
+     * @param controller
+     */
+    public void setController(Controller controller) {
+        this.controller = controller;
+    }
+    
+    @FXML
+    private void handleButtonAction(ActionEvent event) {
+        System.out.println("You clicked me!");
+        label.setText("Hello World!");
+    }
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }
+}
