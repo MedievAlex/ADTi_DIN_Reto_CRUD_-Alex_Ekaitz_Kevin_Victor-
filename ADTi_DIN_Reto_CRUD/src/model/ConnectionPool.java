9f429cacd68a56d4c39680116b0b99ca7a405b27
Ekaitz Campo <ekaitzcampo2002@gmail.com>
2025-10-19 12:54:05 +0200

    Adding sql statements and changing pool language

--- ADTi_DIN_Reto_CRUD/src/model/ConnectionPool.java
+++ ADTi_DIN_Reto_CRUD/src/model/ConnectionPool.java
@@ -5,33 +5,36 @@ import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.ResourceBundle;
 
-public class ConnectionPool {
-    // Instancia única del pool
-    private static final BasicDataSource dataSource;
-    
-    // Configuración estática del pool
-    static {
-        dataSource = new BasicDataSource();
-        
-        // Cargar configuración desde classConfig.properties
+public class ConnectionPool
+{
+
+    // Pool unique instance
+    private static final BasicDataSource DATASOURCE;
+
+    // Pool static configuration
+    static
+    {
+        DATASOURCE = new BasicDataSource();
+
+        // Import configuration from classConfig.properties file
         ResourceBundle configFile = ResourceBundle.getBundle("model.classConfig");
-        
-        dataSource.setUrl(configFile.getString("Conn"));
-        dataSource.setUsername(configFile.getString("DBUser"));
-        dataSource.setPassword(configFile.getString("DBPass"));
-        dataSource.setDriverClassName(configFile.getString("Driver"));
-        
-        // Parámetros del pool
-        dataSource.setInitialSize(5);      // Conexiones iniciales
-        dataSource.setMaxTotal(10);        // Máximo total de conexiones
-        dataSource.setMinIdle(2);          // Mínimo de conexiones inactivas
-        dataSource.setMaxIdle(5);          // Máximo de conexiones inactivas
-        dataSource.setMaxWaitMillis(10000); // Tiempo máximo para esperar una conexión
+
+        DATASOURCE.setUrl(configFile.getString("Conn"));
+        DATASOURCE.setUsername(configFile.getString("DBUser"));
+        DATASOURCE.setPassword(configFile.getString("DBPass"));
+        DATASOURCE.setDriverClassName(configFile.getString("Driver"));
+
+        // Pool parameters
+        DATASOURCE.setInitialSize(5);      // Start connections
+        DATASOURCE.setMaxTotal(10);        // Max total connextions
+        DATASOURCE.setMinIdle(2);          // Min inactive connections
+        DATASOURCE.setMaxIdle(5);          // Max inactive connections
+        DATASOURCE.setMaxWaitMillis(10000); // Max wait time for a connection
     }
 
-
-    // Método para obtener una conexión
-    public static Connection getConnection() throws SQLException {
-        return dataSource.getConnection();
+    // Method to obtain a connection
+    public static Connection getConnection() throws SQLException
+    {
+        return DATASOURCE.getConnection();
     }
 }
--- ADTi_DIN_Reto_CRUD/src/model/DBImplementation.java
+++ ADTi_DIN_Reto_CRUD/src/model/DBImplementation.java
@@ -5,33 +5,52 @@ import java.sql.*;
 /**
  * @author Alex, Ekaitz, Kevin, Victor
  */
-public class DBImplementation implements ModelDAO {
+public class DBImplementation implements ModelDAO
+{
 
     /**
      * SQL Queries: INSERTS
      */
-    final String SQLINSERT_TEACHINGUNIT = "INSERT INTO TeachingUnit (ACRONIM, TITLE, EVALUATION, DESCRIPTION) VALUES (?, ?, ?, ?)";
+    final String SQLINSERT_PROFILE = "INSERT INTO db_profile () VALUES ()";
+    final String SQLINSERT_USER = "INSERT INTO db_user () VALUES ()";
 
     /**
      * SQL Queries: SELECTS
      */
-    final String SQLSELECT_TEACHINGUNIT = "SELECT * FROM TeachingUnit WHERE acronim = ?";
+    final String SQLSELECT_USERS = "SELECT * FROM db_profile JOIN db_user ON P_ID = U_ID";
+    final String SQLSELECT_USER = "SELECT * FROM db_profile JOIN db_user ON P_ID = U_ID WHERE P_EMAIL = ? OR P_USERNAME = ?";
+    final String SQLSELECT_ADMIN = "SELECT * FROM db_profile JOIN db_admin ON P_ID = A_ID WHERE P_EMAIL = ? OR P_USERNAME = ?";
+
+    /**
+     * SQL Queries: UPDATES
+     */
+    final String SQLUPDATE_PROFILE = "UPDATE db_profile SET P_PASSWORD = ?, P_NAME = ?, P_LASTNAME = ?, P_TELEPHONE = ? WHERE P_ID = ?";
+    final String SQLUPDATE_USER = "UPDATE db_user SET U_GENDER = ?, U_CARD = ? WHERE U_ID = ?";
+    final String SQLUPDATE_ADMIN = "UPDATE db_admin SET A_CURRENT_ACCOUNT = ? WHERE A_ID = ?";
+    
+    /**
+     * SQL Queries: DELETES
+     */
+    final String SQLDELETE_USER = "DELETE FROM db_profile WHERE P_ID = ?";
 
     public void metodoPrueba()
     {
-        try (Connection con = ConnectionPool.getConnection(); PreparedStatement stmt = con.prepareStatement(SQLSELECT_TEACHINGUNIT))
+        try (
+                Connection con = ConnectionPool.getConnection();
+                PreparedStatement stmt = con.prepareStatement(SQLSELECT_USERS))
         {
 
             try (ResultSet rs = stmt.executeQuery())
             {
-                while (rs.next()) {
+                while (rs.next())
+                {
                 }
             }
 
         }
         catch (SQLException ex)
         {
-            
+
         }
     }
 }
